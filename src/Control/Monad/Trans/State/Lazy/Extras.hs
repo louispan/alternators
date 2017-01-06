6267bfe683e48fd8bbdf10512d49cdc28dbec83c
Louis Pan <louis@pan.me>
2017-01-06 20:28:24 +1100

    added runReadM'

--- src/Control/Monad/Trans/State/Lazy/Extras.hs
+++ src/Control/Monad/Trans/State/Lazy/Extras.hs
@@ -5,6 +5,9 @@ import Control.Monad.Trans.Except
 import Control.Monad.Trans.Maybe
 import Control.Monad.Trans.State.Lazy
 
+-- | The problem with @StateT s (MaybeT m) a@ is that on failure, the original state is lost.
+-- A more useful type is @MaybeT (StateT s m) a@ which at least keeps the original input state
+-- on failure.
 maybeState :: Monad m => StateT s (MaybeT m) a -> MaybeT (StateT s m) a
 maybeState sm = MaybeT $ do
     s <- get
@@ -15,6 +18,9 @@ maybeState sm = MaybeT $ do
             put s'
             pure (Just a)
 
+-- | The problem with @StateT s (ExceptT e m) a@ is that on failure, the original state is lost.
+-- A more useful type is @ExceptT e (StateT s m) a@ which at least keeps the original input state
+-- on failure.
 exceptState :: Monad m => StateT s (ExceptT e m) a -> ExceptT e (StateT s m) a
 exceptState sm = ExceptT $ do
     s <- get
