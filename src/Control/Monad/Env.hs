3083437380eb9e4500019c0be9f2203de5ddc22d
Louis Pan <louis@pan.me>
2019-05-02 05:16:01 +1000

    added functional dependencies for MonadEnv MonadObserver with unary type envs

--- src/Control/Monad/Env.hs
+++ src/Control/Monad/Env.hs
@@ -9,15 +9,24 @@ import Control.Monad.Reader
 import Control.Monad.Morph
 
 -- | A copy of 'MonadReader' with overlapping instances
-class Monad m => MonadEnv r m | m -> r where
+class Monad m => MonadEnv r m where
     askEnv :: m r
-    localEnv :: (r -> r) -> m a -> m a
+    -- localEnv :: (r -> r) -> m a -> m a
 
-instance {-# OVERLAPPABLE #-} (Monad (t m), MonadTrans t, MFunctor t, MonadEnv r m) => MonadEnv r (t m) where
+instance {-# OVERLAPPABLE #-} (Monad (t m), MonadTrans t, MonadEnv r m) => MonadEnv r (t m) where
     askEnv = lift askEnv
-    localEnv f m = hoist (localEnv f) m
+    -- localEnv f m = hoist (localEnv f) m
 
 instance {-# OVERLAPPABLE #-} Monad m => MonadEnv r (ReaderT r m) where
     askEnv = ask
-    localEnv = local
+    -- localEnv = local
 
+-- | A copy of 'MonadReader' with overlapping instances
+class Monad m => MonadEnv' f r m | m -> r where
+    askEnv' :: m (f r)
+
+instance {-# OVERLAPPABLE #-} (Monad (t m), MonadTrans t, MonadEnv' f r m) => MonadEnv' f r (t m) where
+    askEnv' = lift askEnv'
+
+instance {-# OVERLAPPABLE #-} Monad m => MonadEnv' f r (ReaderT (f r) m) where
+    askEnv' = ask
--- src/Control/Monad/Observer.hs
+++ src/Control/Monad/Observer.hs
@@ -6,8 +6,11 @@
 module Control.Monad.Observer where
 
 import Control.Monad.Reader
+import Data.Maybe
 
-class Monad m => MonadObserver a m | m -> a where
+-- | There is no functional dependency @m -> a@ so it allows multiple uses of
+-- MonadObserver to observer different things in the one function.
+class Monad m => MonadObserver a m where
     askObserver :: m (a -> m ())
 
 instance {-# OVERLAPPABLE #-} (Monad (t m), MonadTrans t, MonadObserver a m) => MonadObserver a (t m) where
@@ -19,3 +22,16 @@ instance {-# OVERLAPPABLE #-} Monad m => MonadObserver a (ReaderT (a -> m ()) m)
 observe :: MonadObserver a m => a -> m ()
 observe a = askObserver >>= ($ a)
 
+-- | There is a functional dependency @m -> a@ to prevent ambiguity in overlapping instances.
+-- This means there can only be one observer at a time for each unique @f@.
+class Monad m => MonadObserver' f a m | m -> a where
+    askObserver' :: m (f a -> m ())
+
+instance {-# OVERLAPPABLE #-} (Monad (t m), MonadTrans t, MonadObserver' f a m) => MonadObserver' f a (t m) where
+    askObserver' = lift $ (lift .) <$> askObserver'
+
+instance {-# OVERLAPPABLE #-} Monad m => MonadObserver' f a (ReaderT (f a -> m ()) m) where
+    askObserver' = (lift .) <$> ask
+
+observe' :: MonadObserver' f a m => f a -> m ()
+observe' a = askObserver' >>= ($ a)
