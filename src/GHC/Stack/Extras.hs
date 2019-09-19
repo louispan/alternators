1b383fc15d3f4f18a66d25fc326b1d8baf0b4a79
Louis Pan <louis@pan.me>
2019-09-20 05:07:00 +1000

    Added callstack pretty functions

--- src/GHC/Stack/Extras.hs
+++ src/GHC/Stack/Extras.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE ImplicitParams #-}
+{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RankNTypes #-}
-
+{-# LANGUAGE RecordWildCards #-}
 
 module GHC.Stack.Extras
 ( module GHC.Stack
@@ -8,6 +9,28 @@ module GHC.Stack.Extras
 ) where
 
 import GHC.Stack
+import Data.String
+import qualified Data.List as L
+
+-- Based on 'GHC.Stack.prettyCallstack'
+prettyCallStack' :: (Semigroup str, IsString str) => str -> [(String, SrcLoc)] -> Maybe str
+prettyCallStack' delim cs = case cs of
+    [] -> Nothing
+    xs -> Just . foldr (<>) "" . L.intersperse delim $ prettyCallSite' <$> xs
+
+trimmedCallstack :: Maybe Int -> [(String, SrcLoc)] -> [(String, SrcLoc)]
+trimmedCallstack depth cs = maybe cs (`take` cs) depth
+
+prettyCallSite' :: (Semigroup str, IsString str) => (String, SrcLoc) -> str
+prettyCallSite' (f, loc) = fromString f <> "@" <> prettySrcLoc' loc
+
+prettySrcLoc' :: (Semigroup str, IsString str) => SrcLoc -> str
+prettySrcLoc' SrcLoc {..}
+    = foldr (<>) "" $ L.intersperse ":"
+        [ fromString srcLocModule
+        , fromString $ show srcLocStartLine
+        , fromString $ show srcLocStartCol
+        ]
 
 -- | Perform some computation excluding the caller's 'CallStack'
 -- This function actually pops two entries from the callstack
