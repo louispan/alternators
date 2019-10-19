4b9b927c6e70bf43ac78656c0fe6834d2e1ff21b
Louis Pan <louis@pan.me>
2019-10-19 11:12:44 +1100

    Renamed Context to Environ

--- src/Control/Monad/Environ.hs
+++ src/Control/Monad/Environ.hs
@@ -8,7 +8,7 @@
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE UndecidableInstances #-}
 
-module Control.Monad.Context where
+module Control.Monad.Environ where
 
 import Control.Monad.Reader
 import Control.Monad.RWS.Lazy as Lazy
@@ -19,56 +19,56 @@ import Control.Monad.State.Strict as Strict
 -- | like 'MonadReader' but with overlapping instances
 -- Use newtype wrappers or 'Tagged' to ensure unique @r@ in the monad stack.
 class Monad m => MonadAsk r m where
-    askContext :: m r
+    askEnviron :: m r
 
 -- | Any transformer on top of 'MonadAsk' is also a 'MonadAsk'
 instance {-# OVERLAPPABLE #-} (Monad (t m), MonadTrans t, MonadAsk r m) => MonadAsk r (t m) where
-    askContext = lift askContext
+    askEnviron = lift askEnviron
 
 instance {-# OVERLAPPABLE #-} Monad m => MonadAsk r (ReaderT r m) where
-    askContext = ask
+    askEnviron = ask
 
 instance {-# OVERLAPPABLE #-} Monad m => MonadAsk r (Strict.StateT r m) where
-    askContext = get
+    askEnviron = get
 
 instance {-# OVERLAPPABLE #-} Monad m => MonadAsk r (Lazy.StateT r m) where
-    askContext = get
+    askEnviron = get
 
 instance {-# OVERLAPPABLE #-} (Monoid w, Monad m) => MonadAsk r (Strict.RWST r w s m) where
-    askContext = ask
+    askEnviron = ask
 
 instance {-# OVERLAPPABLE #-} (Monoid w, Monad m) => MonadAsk r (Lazy.RWST r w s m) where
-    askContext = ask
+    askEnviron = ask
 
 instance {-# OVERLAPPABLE #-} (Monoid w, Monad m) => MonadAsk s (Strict.RWST r w s m) where
-    askContext = get
+    askEnviron = get
 
 instance {-# OVERLAPPABLE #-} (Monoid w, Monad m) => MonadAsk s (Lazy.RWST r w s m) where
-    askContext = get
+    askEnviron = get
 
 -- | like 'MonadState' but with overlapping instances
 class MonadAsk s m => MonadPut s m where
-    putContext :: s -> m ()
-    -- getContext :: m s
-    -- getContext = askContext (Proxy :: Proxy s)
+    putEnviron :: s -> m ()
+    -- getEnviron :: m s
+    -- getEnviron = askEnviron (Proxy :: Proxy s)
 
 instance {-# OVERLAPPABLE #-} (Monad (t m), MonadTrans t, MonadPut s m) => MonadPut s (t m) where
-    putContext = lift . putContext
+    putEnviron = lift . putEnviron
 
 instance {-# OVERLAPPABLE #-} Monad m => MonadPut s (Strict.StateT s m) where
-    putContext = put
+    putEnviron = put
 
 instance {-# OVERLAPPABLE #-} Monad m => MonadPut s (Lazy.StateT s m) where
-    putContext = put
+    putEnviron = put
 
 instance {-# OVERLAPPABLE #-} (Monoid w, Monad m) => MonadPut s (Strict.RWST r w s m) where
-    putContext = put
+    putEnviron = put
 
 instance {-# OVERLAPPABLE #-} (Monoid w, Monad m) => MonadPut s (Lazy.RWST r w s m) where
-    putContext = put
+    putEnviron = put
 
--- | Using ScopedTypeVariables to specify the type of @s@ into 'askContext'
-modifyContext :: forall s m. MonadPut s m => (s -> s) -> m ()
-modifyContext f = do
-    s <- askContext @s
-    putContext $! f s
+-- | Using ScopedTypeVariables to specify the type of @s@ into 'askEnviron'
+modifyEnviron :: forall s m. MonadPut s m => (s -> s) -> m ()
+modifyEnviron f = do
+    s <- askEnviron @s
+    putEnviron $! f s
